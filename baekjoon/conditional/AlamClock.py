c267d074024cc613081fa694f81c45c0be83136d
cksdn43 <101384306+cksdn43@users.noreply.github.com>
2022-06-12 12:13:32 +0900

    Update AlamClock.py

--- baekjoon/conditional/AlamClock.py
+++ baekjoon/conditional/AlamClock.py
@@ -1,2 +1,20 @@
 time = input().split()
 time[0], time[1]= int(time[0]), int(time[1])
+
+if time[1] >= 60:
+  time[0] += time[1] // 60
+  time[1] %= 60
+ 
+if time[0] >= 24:
+  time[0] -= 24
+ 
+time[1] -= 45
+
+if time[1] < 0:
+  time[0] -= 1
+  time[1] += 60
+if time[0] < 0:
+  tiem[0] += 24
+
+ print(time[0], time[1])
+
