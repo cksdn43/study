6bf1de1cf209a2b127c2d66a8ff66431cc82bb33
Cha_nu <101384306+cksdn43@users.noreply.github.com>
2022-11-11 20:56:05 +0900

    Add files via upload

---
+++ 4.Baekjoon/python/10801~10900/10807.py
@@ -0,0 +1,10 @@
+n = input()
+L = list(map(int, input().split()))
+v = int(input())
+cnt = 0
+
+for i in L:
+    if i == v:
+        cnt += 1
+
+print(cnt)
---
+++ 4.Baekjoon/python/2501~2600/2566.py
@@ -0,0 +1,17 @@
+# 최댓값
+
+A = [list(map(int, input().split()))for _ in range(9)]
+result = 0
+c = 0
+
+for i in range(9):
+    a = max(A[i])
+    if a > result:
+        result = a
+        b = i
+for i in range(9):
+    if A[b][i] == result:
+        c = i
+
+print(result)
+print(b+1, c+1)
